04322f2018a5f4d1375ee7d4b94ffa7c517e83c6
Rishikesh BANORE <R.Banore@iaea.org>
2022-02-25 20:27:04 +0100

    "Adding solution"

---
+++ HackerRank_Certification_C_Sharp/EmployeeManagement.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank_Certification_C_Sharp
+{
+    public class EmployeeManagement
+    {
+        public static Dictionary<string, int> AverageAgeForEachCompany(List<Employee> employees)
+        {
+            Dictionary<string, int> EmployeesDict = new Dictionary<string, int>();
+
+            foreach (var e in employees)
+            {
+
+            }
+            return EmployeesDict;
+        }
+
+        public static Dictionary<string, int> CountOfEmployeesForEachCompany(List<Employee> employees)
+        {
+
+        }
+
+        public static Dictionary<string, Employee> OldestAgeForEachCompany(List<Employee> employees)
+        {
+
+        }
+
+        public static void Main()
+        {
+            int countOfEmployees = int.Parse(Console.ReadLine());
+
+            var employees = new List<Employee>();
+
+            for (int i = 0; i < countOfEmployees; i++)
+            {
+                string str = Console.ReadLine();
+                string[] strArr = str.Split(' ');
+
+                employees.Add(new Employee
+                {
+                    FirstName = strArr[0],
+                    LastName = strArr[1],
+                    Company = strArr[2],
+                    Age = int.Parse(strArr[3])
+                });
+            }
+
+            foreach (var emp in AverageAgeForEachCompany(employees))
+            {
+                Console.WriteLine($"The average age for company {emp.Key} is {emp.Value}");
+            }
+
+            foreach (var emp in CountOfEmployeesForEachCompany(employees))
+            {
+                Console.WriteLine($"The count of employees for company {emp.Key} is {emp.Value}");
+            }
+
+            foreach (var emp in OldestAgeForEachCompany(employees))
+            {
+                Console.WriteLine($"The oldest employee of company {emp.Key} is {emp.Value.FirstName} {emp.Value.LastName} having age {emp.Value.Age}");
+            }
+        }
+    }
+
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Company { get; set; }
+    }
+}
